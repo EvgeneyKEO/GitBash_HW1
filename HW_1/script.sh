c80f4e9d6acf7cce3e1cb39d74bb87e543013fee
EvgeneyKEO <117481317+EvgeneyKEO@users.noreply.github.com>
2022-11-12 18:03:07 +0300

    add new file

---
+++ HW_1/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd folder_2/
+mkdir papochka_1 papochka_2 papochka_3
+cd papochka_1/
+touch file1.txt file2.txt file3.txt file4.json file5.json
+mkdir papochka_4 papochka_5 papochka_6
+ls -la
+mv file3.txt file4.json papochka_4/
